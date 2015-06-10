2ac8292848c6fac0bee0f1829484044975338aa7
Rozov Arseniy <a.rozov@2Gis.ru>
2015-06-10 18:00:30 +0600

    fix hardcoded uid, gid, adb command

--- daemonize_adb.sh
+++ daemonize_adb.sh
@@ -2,27 +2,31 @@
 
 SERVICE_NAME=adb-server
 UPSTART_CONF_DIR=/etc/init
+GROUP=`id -gn $USER`
+ADB=`which adb`
 
 echo "Trying to create service \"$SERVICE_NAME\"..."
 if [ -f ${UPSTART_CONF_DIR}/${SERVICE_NAME}.conf ]
 then
-    echo "There is a service with such name already. Change SERVICE_NAME in $0"
+    echo "Service with such name already exists. Change SERVICE_NAME in $0"
     exit 1
 fi
 
-echo 'description "adb-server daemon"
-setuid v4android
-setgid v4android
+sudo env ADB=$ADB USER=$USER GROUP=$GROUP bash << EOF
+echo "description \"adb-server daemon\"
+setuid $USER
+setgid $GROUP
 start on runlevel [2345]
 stop on runlevel [06]
 
 script
-    /home/v4android/Android/Sdk/platform-tools/adb kill-server
-    /home/v4android/Android/Sdk/platform-tools/adb -a fork-server server
+    $ADB kill-server
+    $ADB -a fork-server server
 end script
 
 respawn
-respawn limit 10 90' > ${UPSTART_CONF_DIR}/${SERVICE_NAME}.conf
+respawn limit 10 90" > ${UPSTART_CONF_DIR}/${SERVICE_NAME}.conf
 
 [ $? == 0 ] || exit 1
 echo "Service \"$SERVICE_NAME\" successfully created"
+EOF
